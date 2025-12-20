78668de3639749ecc29430cb2b111ca4f54c6f5e
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-12-20 13:43:58 +0530

    add drag functionality in todo, in progress, and Done

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ const todo = document.querySelector('#todo')
 const progress = document.querySelector('#progress')
 const done = document.querySelector('#done')
 
+// console.log(todo, progress, done);   
+
+const task = document.querySelectorAll('.task')
+
+task.forEach(task => {
+    task.addEventListener("drag", (e)=>{
+        // console.log("dragging ", e);
+        
+    })
+})
+
+// Drag enter 
+
+progress.addEventListener("dragenter", (e)=>{
+    progress.classList.add("hover-over")
+})
+done.addEventListener("dragenter", (e)=>{
+    done.classList.add("hover-over")
+})
+todo.addEventListener("dragenter", (e)=>{
+    todo.classList.add("hover-over")
+})
+
